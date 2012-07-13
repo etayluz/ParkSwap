4358d5d18b357559f9f5679c216e5e9a983a9d36
Etay Luz <etayluz@gmail.com>
2012-07-12 23:59:12 -0700

    Commit

--- Backend/myapp.rb
+++ Backend/myapp.rb
@@ -2,4 +2,14 @@ class MyApp < Sinatra::Base
   get '/' do
     'Hello world!'
   end
+
+  post '/form' do  
+    "You said '#{params[:message]}'"  
+  end
+
+  get '/hello/:name' do  
+    params[:name]  
+  end 
 end
+
+
