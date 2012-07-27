227a2a67ba896c3dbb38bdfb251d870f81b8dd38
Etay Luz <etayluz@gmail.com>
2012-07-27 00:38:46 -0700

    Backend Database Works

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  resources :locations
+
   resources :microposts
 
   resources :users
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726055026) do
+ActiveRecord::Schema.define(:version => 20120727073444) do
+
+  create_table "locations", :force => true do |t|
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
